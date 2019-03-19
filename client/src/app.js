83a77747b0bc38d84ac1f427600237678224ac16
Vyacheslav <vyacheslav.code@gmail.com>
2019-03-19 21:16:47 +0200

    Initial commit

---
+++ client/src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeverProektApp from './components/SeverProektApp'
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<SeverProektApp/>, document.getElementById('app'));
---
+++ server/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const config = require('./db');
+
+const users = require('./routes/user');
+
+mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+    () => {console.log('Database is connected') },
+    err => { console.log('Can not connect to the database'+ err)}
+);
+
+const app = express();
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.use(passport.initialize());
+require('./passport')(passport);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/api/users', users);
+
+app.get('/', function(req, res) {
+    res.send('hello');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
+});
